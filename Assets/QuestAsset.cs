6d9ee1fb43497667b288bafdfcddf4cfef5f41ae
baobao <shunsuke.ohba@gmail.com>
2020-07-31 10:21:37 +0900

    add test code

---
+++ Assets/QuestAsset.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class QuestAsset : ScriptableObject
+{
+    public string id;
+}
---
+++ Assets/Tests/Editor/AssetTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using UnityEditor;
+using Assert = UnityEngine.Assertions.Assert;
+
+/// <summary>
+/// TestRunner AssetTest
+/// </summary>
+public class AssetTest
+{
+
+    [Test]
+    public void AssetValidate()
+    {
+        // Don't stop the process with Assert
+        Assert.raiseExceptions = false;
+
+        var assetDirectoryPath = "Assets/AssetBundles/";
+        var filePaths = Directory.GetFiles(assetDirectoryPath, "*.asset");
+
+        foreach (var path in filePaths)
+        {
+            Validate(path);
+        }
+    }
+
+    private void Validate(string path)
+    {
+        var fileName = Path.GetFileName(path);
+        var asset = AssetDatabase.LoadAssetAtPath<QuestAsset>(path);
+
+        Assert.IsNotNull(asset, $"{fileName} => asset is null");
+
+        Assert.IsFalse(string.IsNullOrEmpty(asset.id),
+            $"{fileName} => ID is null or empty");
+
+        Assert.IsFalse(asset.id.IndexOf("A", StringComparison.Ordinal) != 0,
+            $"{fileName} => The naming conventions are different. : {asset.id}");
+    }
+}
+
+// Unity.app -runTests -projectPath UnityTestRunnerAssetTestShortcode -batchmode -testPlatform EditMode
